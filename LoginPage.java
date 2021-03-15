2aae57e5b100f59755361c8bb12912aff22ba320
zarif92 <71910876+zarif92@users.noreply.github.com>
2021-03-15 00:00:08 -0400

    Add files via upload

--- LoginPage.java
+++ LoginPage.java
@@ -1,33 +1,39 @@
-package one;
+package cubesoftware;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
 
 public class LoginPage {
+  
+	WebDriver driver;
 	
-		WebDriver driver;
-		
-		By buttonTshirt = By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a");
-		By buttonImage = By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li/div/div[1]/div/a[1]/img");
-		By buttonCart = By.xpath("/html/body/div/div/div[3]/form/div/div[3]/div[1]/p/button/span");
-		
-
-		
-
+	By buttonProduct = By.xpath("/html/body/header/div/div/nav[2]/div[2]/ul/li[1]/a");
+	By buttonSolutions = By.xpath("/html/body/header/div/div/nav[2]/div[2]/ul/li[2]/a");
+	By buttonPricing = By.xpath("/html/body/header/div/div/nav[2]/div[2]/ul/li[3]/a");
+	By buttonBestPractices = By.xpath("/html/body/header/div/div/nav[2]/div[2]/ul/li[4]/a");
+	By buttonCompany = By.xpath("/html/body/header/div/div/nav[2]/div[2]/ul/li[5]/a");
+	
+	
+	
 
 public LoginPage(WebDriver driver){
 	  this.driver = driver;
 }
-public void clickButtontshirt () {
-	driver.findElement(buttonTshirt).click();
+	  
+public void clickButtonproduct () {
+driver.findElement(buttonProduct).click();
 }
-public void clickButtonimage () {
-	driver.findElement(buttonImage).click();
+public void clickButtonsolutions () {
+driver.findElement(buttonSolutions).click();
 }
-public void clickButtoncart () {
-	driver.findElement(buttonCart).click();
-	
+public void clickButtonpricing () {
+driver.findElement(buttonPricing).click();
+}
+public void clickButtonbestpractices () {
+driver.findElement(buttonBestPractices).click();
+}
+public void clickButtoncompany () {
+driver.findElement(buttonCompany).click();
+}
 }
 
-}
