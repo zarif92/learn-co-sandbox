7b84028cc345aed849a226ff0ef54c0b5743b4f2
zarif92 <71910876+zarif92@users.noreply.github.com>
2021-03-15 00:01:00 -0400

    Add files via upload

--- LoginTest.java
+++ LoginTest.java
@@ -1,36 +1,48 @@
-package one;
+package cubesoftware;
+
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 
 public class LoginTest {
+  
+	
+	@Test
+  
+	public void menuSelection() {
+	
+  String baseURL = "https://www.cubesoftware.com/";
+	
+  System.setProperty("webdriver.chrome.driver", "/Users/mac/Downloads/chromedriver");
+  WebDriver driver = new ChromeDriver();
+  driver.navigate().to(baseURL);
+  driver.manage().window().maximize();
+ 
+   LoginPage login = new LoginPage(driver);
+		login.clickButtonproduct();
+		login.clickButtonsolutions();
+		login.clickButtonpricing();
+	  
+		driver.quit();
+	}
+	@AfterTest
 
-   			  
-		
-	  @Test
-	  public void buyItem() {
-		  
-		
-
-		  String baseURL = "http://automationpractice.com/index.php";
+	public void secondMenu() {
+	String baseURL = "https://www.cubesoftware.com/";
+	
+	System.setProperty("webdriver.chrome.driver", "/Users/mac/Downloads/chromedriver");
+	WebDriver driver = new ChromeDriver();
+	driver.navigate().to(baseURL);
+    driver.manage().window().maximize();
+     
+     LoginPage login = new LoginPage(driver);
+		login.clickButtonbestpractices();
+		login.clickButtoncompany();
+	  
+		driver.quit();
+}
 			
-		  System.setProperty("webdriver.chrome.driver", "/Users/mac/Downloads/chromedriver");
-			WebDriver driver = new ChromeDriver();
-			driver.navigate().to(baseURL);
-	       driver.manage().window().maximize();
-	       //driver.get("http://automationpractice.com/index.php");
-		  
-	       LoginPage login = new LoginPage(driver);
-			login.clickButtontshirt();
-			login.clickButtonimage();
-			login.clickButtoncart();
-		  
-			driver.quit();
-  }
-
-  }
-
+	}
 
